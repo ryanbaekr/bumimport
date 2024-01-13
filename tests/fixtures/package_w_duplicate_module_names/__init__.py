64c4560cd953ff1ab3e5d400ed306c98c7392066
Ryan Baker <ryanbaekr@gmail.com>
2024-01-13 14:30:26 -0500

    round 2 of tests

---
+++ tests/fixtures/package_w_duplicate_module_names/__init__.py
@@ -0,0 +1,5 @@
+"""A Python package"""
+
+from bumimport import flat_import as _flat_import
+
+_flat_import(__name__, __file__)
--- tests/test_flat_import.py
+++ tests/test_flat_import.py
@@ -37,3 +37,39 @@ def test_package_w_module_w_stdlib_name() -> None:
     assert mod2.func2() == os_sep
 
     assert os.sep == os_sep
+
+
+def test_package_w_module_w_dot_in_name() -> None:
+    """Test flat_import with a package with a module with a dot in its name"""
+
+    import package_w_module_w_dot_in_name  # pylint: disable=import-outside-toplevel,import-error
+
+    mod1 = getattr(package_w_module_w_dot_in_name, "mod.ule1")
+    assert mod1.func1() == "foo"
+
+    mod2 = getattr(package_w_module_w_dot_in_name, "module2")
+    assert mod2.func2() == "bar"
+
+
+def test_package_w_module_w_dash_in_name() -> None:
+    """Test flat_import with a package with a module with a dash in its name"""
+
+    import package_w_module_w_dash_in_name  # pylint: disable=import-outside-toplevel,import-error
+
+    mod1 = getattr(package_w_module_w_dash_in_name, "mod-ule1")
+    assert mod1.func1() == "foo"
+
+    mod2 = getattr(package_w_module_w_dash_in_name, "module2")
+    assert mod2.func2() == "bar"
+
+
+def test_package_w_duplicate_module_names() -> None:
+    """Test flat_import with a package with multiple modules with the same name"""
+
+    try:
+        import package_w_duplicate_module_names  # pylint: disable=import-outside-toplevel,import-error
+        message = package_w_duplicate_module_names.module1.func1()
+    except Exception as e:  # pylint: disable=broad-exception-caught
+        message = str(e)
+
+    assert message == "Multiple modules named: module1"
